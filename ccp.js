e071abe1dcc2494f0aee8c89f1d3871685c016f8
Svetlana <roksana17.1.kz@gmail.com>
2023-12-29 00:53:59 +0600

    first commit

---
+++ ccp.js
@@ -0,0 +1,36 @@
+const age = 27;
+console.log(typeof age);
+const bigIth = 2555555555555n;
+console.log(typeof bigIth);
+const firstName = 'Name'
+console.log(typeof firstName);
+let age2;
+console.log(typeof age2);
+
+const obj = {
+  name: 'Svetlana',
+  age: 30,
+  isWorked: true
+}
+
+const num = 20
+const num2 = 10
+console.log(num + num2);
+console.log(num - num2);
+console.log(num * num2);
+console.log(num / num2);
+console.log(num % num2);
+
+
+const titleProject = 5000000;
+console.log(typeof titleProject);
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями"
+console.log(typeof screensValue);
+const screenPrice = 3000;
+console.log(typeof screenPrice);
+const percentage = 10;
+console.log(typeof percentage);
+const fullPrice = 2000000;
+console.log(typeof fullPrice);
+const responsive = true;
+console.log(typeof responsive);
